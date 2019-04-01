3c2dccac53e32820c0ff7ea5ee582da4554c194d
evansjames <jamesevans963@gmail.com>
2019-04-01 14:18:50 +0100

    comments

--- public/scripts/eventScripts.js
+++ public/scripts/eventScripts.js
@@ -3,11 +3,12 @@ function newEvent() {
         document.getElementById('postalCode').value + ' ' + document.getElementById('town').value + ' ' +
         document.getElementById('city').value + ' ' + document.getElementById('country').value;
     console.log(address);
-    var formArray= $("form").serializeArray();
-    var data={};
-    for (index in formArray){
+    var formArray = $("form").serializeArray();
+    var data = {};
+    for (index in formArray) {
         data[formArray[index].name] = formArray[index].value;
     }
+
     data['eventLocation'] = address;
     console.log(formArray);
     sendAjaxQuery('/post_event', data);
@@ -20,7 +21,7 @@ function sendAjaxQuery(url, data) {
         data: data,
         dataType: 'json',
         type: 'POST',
-        success: function (dataR) {
+        success: function(dataR) {
             // no need to JSON parse the result, as we are using
             // dataType:json, so JQuery knows it and unpacks the
             // object for us before returning it
@@ -29,7 +30,7 @@ function sendAjaxQuery(url, data) {
             // we need to JSON stringify the object;
             //document.getElementById('results').innerHTML= JSON.stringify(dataR);
         },
-        error: function (xhr, status, error) {
+        error: function(xhr, status, error) {
             alert('Error: ' + error.message);
         }
     });
@@ -37,22 +38,19 @@ function sendAjaxQuery(url, data) {
 
 function fileSystemPhoto() {
     var preview = document.querySelector(".eventImagePreview");
-    var file    = document.querySelector('input[type=file]').files[0];
-    var field   = document.getElementById('eventPhoto');
-    var reader  = new FileReader();
+    var file = document.querySelector('input[type=file]').files[0];
+    var field = document.getElementById('eventPhoto');
+    var reader = new FileReader();
 
-    reader.addEventListener("load", function () {
+    reader.addEventListener("load", function() {
         console.log(reader.result);
         preview.src = reader.result;
         field.value = reader.result;
     }, false);
-
     if (file) {
         reader.readAsDataURL(file);
     }
 }
-
-
 
 function addToEventList(dataR) {
     if (document.getElementById('eventList') != null) {
@@ -69,8 +67,9 @@ function addToEventList(dataR) {
         document.getElementById('eventList').appendChild(row);
         row.appendChild(body);
 
-        body.innerHTML = "<a class=\"eventNameResult\" href=view_event/" + dataR.id + ">" + dataR.eventName + ", "  + dataR.eventLocation + " " + dataR.eventDate + "</a>"
-        body.innerHTML += "<div class='card-story-counter red'>&nbsp; <span class=\"w3-badge w3-red\">"+dataR.id+"</span></div> <div class=\"card-story-counter\">\n" +
+        // add html to the event cards
+        body.innerHTML = "<a class=\"eventNameResult\" href=view_event/" + dataR.id + ">" + dataR.eventName + ", " + dataR.eventLocation + " " + dataR.eventDate + "</a>"
+        body.innerHTML += "<div class='card-story-counter red'>&nbsp; <span class=\"w3-badge w3-red\">" + dataR.id + "</span></div> <div class=\"card-story-counter\">\n" +
             "        Stories:\n" +
             "      </div>"
     }
@@ -78,97 +77,91 @@ function addToEventList(dataR) {
 
 function noEventResults() {
     var row = document.createElement('div');
-
     document.getElementById('eventList').appendChild(row);
     row.classList.add('card');
     row.classList.add('card-header');
+    // inside card information
     row.innerHTML = "😥 No current events"
-
 }
-
 
 // store all the locations from the event
 var address_array = [];
 
 // update the google map with new events
-function updateMap(dataR, thing2) {
-    var thing3 = thing2;
+function updateMap(dataR, original_data) {
+    var dataJ = original_data;
     var name_array = [];
     var index_array = [];
 
-    var row = document.createElement('div');
     var body = document.createElement('div');
-    var name;
 
     document.getElementById('map').remove();
     document.getElementById('james_is_the_best').appendChild(body);
     body.innerHTML = "<div id='map'></div>";
 
     var i;
-    for (i = 0; i < thing2.length; i++) {
-        address_array.push(thing2[i].eventLocation);
-        name_array.push(thing2[i].eventName);
-        index_array.push(thing2[i].id)
+    for (i = 0; i < original_data.length; i++) {
+        address_array.push(original_data[i].eventLocation);
+        name_array.push(original_data[i].eventName);
+        index_array.push(original_data[i].id);
     }
-
-    initMap(thing3);
+    // start the google maps api
+    initMap(dataJ);
 
     var map;
-    function initMap(thing3) {
+
+    function initMap(dataJ) {
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 53, lng: 1},
+            center: {
+                lat: 53,
+                lng: 1
+            },
             zoom: 12
         });
 
         var geocoder = new google.maps.Geocoder();
 
-        geocodeAddress(geocoder, map, thing3);
+        geocodeAddress(geocoder, map, dataJ);
     }
-
-    function geocodeAddress(geocoder, resultsMap, thing3) {
-
-
+    // convert raw address name into lat lng
+    function geocodeAddress(geocoder, resultsMap, dataJ) {
 
         var image = {
-            url: '/images/icons/icon.png',
+            url: '/images/icons/icon.png'
         };
 
-
-        var labels = ['cat', 'dog', 'chicken'];
-        var string = []
         var labelIndex = 0;
         var newIndex = 0;
-        //var address = 'kt152er';
-        for (p = 0; p < address_array.length; p++) {
+        var p;
 
-            geocoder.geocode({'address': address_array[p]}, function(results, status) {
+        // for every event in the database
+        for (p = 0; p < address_array.length; p++) {
+            geocoder.geocode({
+                'address': address_array[p]
+            }, function(results, status) {
 
                 if (status === 'OK') {
 
-                    resultsMap.setCenter(results[0].geometry.location);
-
-
+                    // track the index and labels of the events as they will be seperated
                     var index_value = index_array[labelIndex++ % index_array.length];
                     var label_value = name_array[newIndex++ % name_array.length];
-                    var contentString = "<a href=view_event/" + index_value + ">" + label_value + "</a>"
+                    var contentString = "<a href=view_event/" + index_value + ">" + label_value + "</a>";
 
                     var infowindow = new google.maps.InfoWindow({
                         content: contentString
                     });
 
-
-
+                    // add on a marker for every result
                     var marker = new google.maps.Marker({
                         map: resultsMap,
                         position: results[0].geometry.location,
                         animation: google.maps.Animation.DROP,
                         icon: image
-                        //icon: image,
                     });
+                    // add a listener for the event title to appear
                     marker.addListener('click', function() {
                         infowindow.open(map, marker);
                     });
-
                 } else {
                     alert('Geocode was not successful for the following reason: ' + status);
                 }
@@ -176,39 +169,43 @@ function updateMap(dataR, thing2) {
         }
 
         navigator.geolocation.getCurrentPosition(function(position) {
+
+            // find current user location
             var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
 
+            // fetch new icon for current user location
             var image = {
-                url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-
+                url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
             };
 
-            var myLatLng = {lat: position.coords.latitude, lng: position.coords.longitude};
+            var myLatLng = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
             var marker = new google.maps.Marker({
                 map: map,
                 icon: image,
-                position: myLatLng,
+                position: myLatLng, // center the map on the users location
 
                 animation: google.maps.Animation.DROP
             });
 
             var contentString = "You are here."
 
+            // create new window for event title can go in
             var infowindow = new google.maps.InfoWindow({
                 content: contentString
             });
-
+            // when event pin clicked open the window
             marker.addListener('click', function() {
                 infowindow.open(map, marker);
             });
-
+            // center the map on the user
             map.setCenter(pos);
         });
-
-
     }
 
     function toggleBounce() {
@@ -224,8 +221,9 @@ function updateResults(content) {
     $('.eventNameResult').parent().hide();
 
     // Search and show
-    $('.eventNameResult:contains("'+content+'")').parent().show();
+    $('.eventNameResult:contains("' + content + '")').parent().show();
 }
+
 function displayEvent(dataR) {
 
     if (dataR.eventName != null) {
@@ -237,19 +235,18 @@ function displayEvent(dataR) {
     var header = document.getElementById('masthead_id');
 
     if (dataR.eventPhoto != null) {
-        header.style.background = "url('"+dataR.eventPhoto+"')";
+        // use event header
+        header.style.background = "url('" + dataR.eventPhoto + "')";
     } else {
+        // use placeholder header
         header.style.background = "url('/images/placeholder_img_prev.jpg')";
     }
-
+    // fetch story information
     document.getElementById('address').innerHTML = dataR.eventLocation;
     document.getElementById('date').innerHTML += dataR.eventDate;
     document.getElementById('smaller_title_date').innerHTML = dataR.eventDate;
     document.getElementById('address').innerHTML += "<br><hr><i>" + dataR.eventDescription + "</i>";
-    //document.getElementById('title_of_event_2').innerHTML = dataR.eventLocation;
-    //document.getElementById('photoHeader').src = dataR.eventPhoto;
 
-
+    // fetch information associated with
     getAllStoryData(dataR.id);
 }
-
