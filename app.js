121bfc2a9cb32f68c538b7853cf0d5420f14cd9c
Joseph Eid <jceid1@sheffield.ac.uk>
2019-05-15 01:53:56 +0100

    socketio set up

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ var eventsRouter = require('./routes/events');
 var storiesRouter = require('./routes/stories');
 
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+server.listen(3001);
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world'});
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
